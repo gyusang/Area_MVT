a92c8f7613373b959685efc9b7c235fab4376363
Sanggyu Lee <sanggyu523@naver.com>
2018-06-17 21:30:53 +0900

    Find two half

--- app.py
+++ app.py
@@ -4,13 +4,9 @@ from matplotlib import lines as mlines
 from descartes import PolygonPatch
 import numpy as np
 
+error = 0.0001
 
-
-#def plot_line(ax, ob):
-#    x, y = ob.xy
-#    ax.plot(x, y) #, alpha=0.7, linewidth=3, solid_capstyle='round', zorder=2)
-
-def new_line(ax,x,y):
+def new_line(ax,x,y): #plot 내에 꽉차는 직선(선분)을 그리는 함수, 점 두 개를 입력받음
     xmin, xmax = ax.get_xbound()
 
     if(x[0] == x[1]):
@@ -24,14 +20,14 @@ def new_line(ax,x,y):
     ax.add_line(l)
     return l
 
-def plot_line(ax, ob):
+def plot_line(ax, ob):#직선 그리는 함수, 직선 객체를 입력받음.
     x, y = ob.xy
     new_line(ax,x,y)
 
-def draw_poly(ax, ob):
-    ax.add_patch(PolygonPatch(ob,alpha=0.8))
+def draw_poly(ax, plg, c='#0F00FF'):#다각형 그리는 함수, 다각형 점 목록 리스트를 입력받음
+    ax.add_patch(PolygonPatch(Polygon(plg),alpha=0.6,color=c))
 
-def sign(i):
+def sign(i): #부호를 반환하는 함수, 양수면 1, 음수면 -1, 영이면 0
     if i>0:
         return 1
     elif i<0:
@@ -41,17 +37,15 @@ def sign(i):
     else:
         None
 
-def slope_line(ax,slope,p):
+def slope_line(ax,slope,p): #기울기와 한 점으로 직선을 그리는 함수
     new_line(ax,(p[0],p[0]+1),(p[1],p[1]+slope))
 
-def poly_area(plg,slope,p):#볼록 다각형 가정, plg : polygon points, slope : slope of line, y : y절편
+def poly_area(plg,slope,p):#다각형에서 직선 위 부분의 넓이에서 다각형 넓이의 절반을 뺀 값을 반환하는 함수
     global ax
-    #ax.plot(p[0],p[1],'o')
     y = p[1]-slope*p[0]+0.00000001#점이 일치하면 부등식에 오류가 생기는 것을 방지하기 위해 약간 띄움
     xrange = [min(plg,key=lambda x:x[0])[0],max(plg,key=lambda x:x[0])[0]]
     yrange = [min(plg,key=lambda x:x[1])[1],max(plg,key=lambda x:x[1])[1]]
     line = LineString([(xrange[0],slope*xrange[0]+y),(xrange[1],slope*xrange[1]+y)])
-    #plot_line(ax,line)
     ring = LinearRing(plg)
     inter = line.intersection(ring)
     if inter.is_empty or type(inter)==Point :
@@ -75,27 +69,29 @@ def poly_area(plg,slope,p):#볼록 다각형 가정, plg : polygon points, slope
                 sectors[1].append((inter.x,inter.y))
             sectors[(D-1)//2].append(plg[i])
         status = D
-#    draw_poly(ax,Polygon(sectors[0]))
-#    draw_poly(ax,Polygon(sectors[1]))
     return Polygon(sectors[0]).area-Polygon(plg).area/2
 
-def find_slope_half(plg,slope,ax=None,ay=None):
+def find_b_half(plg,slope,ax=None,ay=None):#이진 탐색으로 주어진 기울기로 다각형을 이등분하는 y절편값을 반환하는 함수
     dist = [k[1]-k[0]*slope for k in plg]
     yrange = [min(dist),max(dist)]
+    if ay:
+        ay.plot(yrange,[poly_area(plg,slope,(0,k))for k in yrange],'o')
     while True:
+        if yrange[1]-yrange[0]<=0.000001:
+            print("Failed to find")
+            return None
         new_half = sum(yrange)/2
         D = poly_area(plg,slope,(0,new_half))
-        if abs(D)<0.001: break
-        elif D>0: yrange[0]=new_half
-        elif D<0: yrange[1]=new_half
         if ay:
             ay.plot(new_half,D,'o')
+        if abs(D)<error: break
+        elif D>0: yrange[0]=new_half
+        elif D<0: yrange[1]=new_half
     if ax:
         slope_line(ax,slope,(0,new_half))
-    print('slope_half error:%d'%D)
     return new_half
 
-def show_slope_graph(plg,slope,ax=None,ay=None):
+def show_b_graph(plg,slope,ax=None,ay=None):#y절편에 따른 poly_area의 값을 그래프로 그리고 최소지점을 반환하는 함수
     dist = [k[1]-k[0]*slope for k in plg]
     yrange = (min(dist),max(dist))
     y = np.arange(yrange[0]-1,yrange[1]+1,0.01)
@@ -103,7 +99,7 @@ def show_slope_graph(plg,slope,ax=None,ay=None):
     if ay:
         ay.plot(y,areas)
     min_area = min(areas,key=lambda x:abs(x))
-    print(min_area)
+    #print(min_area)
     result = y[areas.index(min_area)]
     if ax:
         slope_line(ax,slope,(0,result))
@@ -111,33 +107,106 @@ def show_slope_graph(plg,slope,ax=None,ay=None):
         slope_line(ax,slope,(0,yrange[1]))
     return result
 
-C = (2.94, 2.46)
-D = (2.28, 3.96)
-E = (-0.22, 4.32)
-F = (-0.22, 1.64)
-G = (0.82, 0.66)
+def show_a_graph(plg1,plg2,ax=None,ay=None):
+#기울기에 따른 두 번째 다각형의 분할된 넓이에서
+#두 번째 다각형의 넓이의 절반을 뺀 값을 그리고 그 값이 0에 가장 가까운 직선을 반환하는 함수
+    theta = np.arange(-np.pi/2+0.01,np.pi/2-0.01,0.01)
+    slope = np.tan(theta)
+    areas = [poly_area(plg2,k,(0,find_b_half(plg1,k)))for k in slope]
+    if ay:
+        ay.plot(theta,areas)
+    min_area = min(areas,key=lambda x:abs(x))
+    #print(min_area)
+    result_slope = slope[areas.index(min_area)]
+    #print(theta[areas.index(min_area)])
+    result_b = find_b_half(plg1,result_slope)
+    if ax:
+        slope_line(ax,result_slope,(0,result_b))
+    return (result_slope,result_b)
 
-plg = Polygon([C,D,E,F,G])
-fig = plt.figure('Ham Sandwich Theorem')
-ax = fig.add_subplot(121)
-ay = fig.add_subplot(122)
-ax.set_aspect(1)
-#ay.set_aspect(1)
-ax.set_title('Polygon')
-ay.set_title('Area by Location')
-ax.set_xlim((plg.bounds[0]-1,plg.bounds[2]+1))
-ax.set_ylim((plg.bounds[1]-1,plg.bounds[3]+1))
-draw_poly(ax,plg)
-#X = sorted(list(np.random.triangular(-10,0,10,1000)))
-#p = C
-#Y = [poly_area([C,D,E,F,G],x,p) for x in X]
-#minimum = X[Y.index(min(Y))]
-#slope_line(ax,minimum,p)
+def find_a_half(plg1,plg2,ax=None,ay=None):
+#이진 탐색으로 두 다각형을 동시에 이등분하는 직선을 찾아 반환하는 함수
+    thetarange = [-np.pi/2+0.01,np.pi/2-0.01]
+    areas = [poly_area(plg2,k,(0,find_b_half(plg1,k))) for k in np.tan(thetarange)]
+    if ay:
+        ay.plot(thetarange,areas,'o')
+    if sign(areas[0])==sign(areas[1]) and areas[0]!=0:
+        print("Failed to find")
+        return None
+    elif abs(areas[0]) < error:
+        slope = np.tan(thetarange[0])
+        if ax:
+            slope_line(ax,slope,(0,find_b_half(plg1,slope)))
+        return (slope,find_b_half(plg1,slope))
+    elif abs(areas[1]) < error:
+        slope = np.tan(thetarange[1])
+        if ax:
+            slope_line(ax,slope,(0,find_b_half(plg1,slope)))
+        return (slope,find_b_half(plg1,slope))
+    if areas[0]<0:
+        thetarange.reverse()
+    while True:
+        if abs(thetarange[1]-thetarange[0])<0.000001:
+            print("Failed to find")
+            return None
+        new_half = sum(thetarange)/2
+        slope = np.tan(new_half)
+        D = poly_area(plg2,slope,(0,find_b_half(plg1,slope)))
+        if ay:
+            ay.plot(new_half,D,'o')
+        if abs(D)<error: break
+        elif D<0: thetarange[1] = new_half
+        elif D>0: thetarange[0] = new_half
+    if ax:
+        slope_line(ax,slope,(0,find_b_half(plg1,slope)))
+    #print("D=%f,theta=%f"%(D,new_half))
+    return (slope,find_b_half(plg1,slope))
 
-#print(show_slope_graph([C,D,E,F,G],-1,ax,ay))
-print(find_slope_half([C,D,E,F,G],np.sqrt(31),ax,ay))
-#print(min(Y))
-#result = poly_area([C,D,E,F,G],0,E)
-#print(result)
-#ay.plot(X,Y)
-plt.show()
+if __name__=="__main__":#예제
+    A = (1, 1)#10개의 점을 지정한다
+    B = (1.7, 1.66)
+    C = (4.28, 1.6)
+    D = (3.06, 0.2)
+    E = (1.26, -0.12)
+
+    # F = (1, 3)#평행이동한 경우
+    # G = (1.7, 3.66)
+    # H = (4.28, 3.6)
+    # I = (3.06, 2.2)
+    # J = (1.26, 1.88)
+
+    F = (1.6, 2.94)
+    G = (2.34, 1.34)
+    H = (3.38, 1.14)
+    I = (5.9, 2.18)
+    J = (4, 4)
+
+    plg1 = [A,B,C,D,E]#두 개의 오각형을 만든다
+    plg2 = [F,G,H,I,J]
+
+    # plg1 = [(k[1],k[0])for k in plg1]#y=x 대칭
+    # plg2 = [(k[1],k[0])for k in plg2]
+
+    fig = plt.figure('Ham Sandwich Theorem')#그래프를 설정한다
+    ax = fig.add_subplot(121)
+    ay = fig.add_subplot(122)
+    ax.set_aspect(1)
+    ax.set_title('Polygon')
+    ay.set_title('Area')
+    #plt.xlabel('theta')
+    ax.set_xlim((min(Polygon(plg1).bounds[0],Polygon(plg2).bounds[0])-1,
+    max(Polygon(plg1).bounds[2],Polygon(plg2).bounds[2])+1))
+    ax.set_ylim((min(Polygon(plg1).bounds[1],Polygon(plg2).bounds[1])-1,
+    max(Polygon(plg1).bounds[3],Polygon(plg2).bounds[3])+1))
+
+    draw_poly(ax,plg1,'#27567b')#다각형 두 개를 다른 색으로 그린다
+    draw_poly(ax,plg2,'#b63b3b')
+
+    #실행 예시
+
+    #print(show_b_graph(plg2,-5,ax,ay))
+    #print(find_b_half(plg1,10,ax,ay))
+    #print(show_a_graph(plg1,plg2,ax,ay))
+    print(find_a_half(plg1,plg2,ax,ay))
+
+    plt.show()
